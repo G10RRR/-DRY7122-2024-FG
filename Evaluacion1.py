74110e9f22f1e3be91a956623c9a338bdbcc1aae
G10RRR <jor.gajardo@duocuc.cl>
2024-04-11 19:50:12 +0000

    Añadimos los archivos del grupo a DRY7122-2024-FG

---
+++ Evaluacion1.py
@@ -0,0 +1,4 @@
+print ("Evaluacion N°1 Programación y Redes Virtualizadas")
+print ("nombre de integrantes")
+print ("Fabian Fernandez Retamales")
+print ("Jorge Gajardo Arellano")
---
+++ Evaluacion1input.py
@@ -0,0 +1,11 @@
+nombre = input("Por favor, ingresa tu nombre: ")
+apellido = input("Por favor, ingresa tu apellido: ")
+codigosec = input("Por favor, ingresa tu código de sección: ")
+sede = input("Por favor, ingresa tu sede: ")
+
+# Imprimir la información ingresada
+print("\nInformación ingresada:")
+print("Nombre:", nombre)
+print("Apellido:", apellido)
+print("Código de sección:", codigosec)
+print("Sede:", sede)
